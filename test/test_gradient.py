de762577356bcb1a05a03a567813f2ee3a9fcc79
jackkamm <jackkamm@Amalthea.local>
2015-04-05 10:33:48 -0700

    more work on test_gradient

--- test/test_gradient.py
+++ test/test_gradient.py
@@ -1,11 +1,11 @@
 from __future__ import division
 import pytest
 from ad import gh, adnumber
-from ad.admath import exp
+from ad.admath import exp,log
 import numpy as np
 from size_history import ConstantTruncatedSizeHistory, PiecewiseHistory
 import networkx as nx
-from demography import Demography
+from demography import Demography, normalizing_constant
 import random
 from sum_product import SumProduct
 
@@ -33,7 +33,12 @@ def check_gradient(f, x):
     print x, "\n", f(x)
 
     print grad1, "\n", grad2
-    assert max(np.log(grad1 / grad2)) < 1e-2
+    approx_zero = np.logical_or(np.abs(grad1) < 1e-15, np.abs(grad2) < 1e-15)
+    not_zero = np.logical_not(approx_zero)
+    if np.any(not_zero):
+        assert max(np.abs(np.log(grad1[not_zero] / grad2[not_zero]))) < 1e-2 
+    if np.any(approx_zero):
+        assert np.all(grad1[approx_zero] < 1e-15) and np.all(grad2[approx_zero] < 1e-15)
 
     # avoid doing numerical hessian for now as it is unstable
 #     hess1 = np.asarray(map(np.asarray, fxd.hessian(xd)))
@@ -44,6 +49,17 @@ def check_gradient(f, x):
 def test_simple():
     def f(x):
         return np.sum(np.outer(x,x))
+    check_gradient(f, np.random.normal(size=10))
+
+def test_simple_2():
+    def f(x):
+        return np.sum(1.0 / np.outer(x,x))
+    check_gradient(f, np.random.normal(size=10))
+
+def test_simple_3():
+    def f(x):
+        #return sum(x/x)
+        return sum(x) / sum(x)
     check_gradient(f, np.random.normal(size=10))
 
 def piecewise_constant_demo(x, n_lins):
@@ -85,15 +101,38 @@ def sfs_func(demo_func, n_lins, normalized=True):
     def f(x):
         demo = demo_func(x, n_lins)
         demo.update_state(states)
-        return SumProduct(demo).p(normalized=normalized)
+        #return SumProduct(demo).p(normalized=normalized)
+        ret = SumProduct(demo).p(normalized=False)
+        if normalized:
+            ret = ret / demo.totalSfsSum
+            #ret = log(ret) - log(demo.totalSfsSum)
+        return ret
     return f
 
 @pytest.mark.parametrize("n,epochs,normalized", 
-                         ((5,5,norm) for norm in (False,True)))
+                         ((2,1,norm) for norm in (False,True)))
 def test_piecewise_constant_p(n, epochs, normalized):
     n_lins = {'a' : n}
 
-    x = np.random.normal(size=2*epochs - 1)
+    #x = np.random.normal(size=2*epochs - 1)
+    x = np.zeros(2*epochs-1)
     f = sfs_func(piecewise_constant_demo, n_lins, normalized=normalized)
+
+    check_gradient(f,x)
+
+
+def normalizing_constant_func(demo_func, n_lins):
+    def f(x):
+        demo = demo_func(x, n_lins)
+        return demo.totalSfsSum
+    return f
+
+@pytest.mark.parametrize("n,epochs", 
+                         ((5,5),))
+def test_piecewise_constant_normalizing(n, epochs):
+    n_lins = {'a' : n}
+
+    x = np.random.normal(size=2*epochs - 1)
+    f = normalizing_constant_func(piecewise_constant_demo, n_lins)
 
     check_gradient(f,x)
