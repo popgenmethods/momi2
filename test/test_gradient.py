fixed nondifferentiability of normalizing constant

test/test_gradient.py
@@ -8,8 +8,12 @@ import networkx as nx
 from demography import Demography, normalizing_constant
 import random
 from sum_product import SumProduct
+import math
+from numdifftools import Gradient, Hessian
 
-def num_grad(f,x,eps=1e-4):
+EPS=1e-8
+
+def num_grad(f,x,eps=EPS):
     ret = []
     for i in range(len(x)):
         dx = np.zeros(len(x))
@@ -17,34 +21,41 @@ def num_grad(f,x,eps=1e-4):
         ret.append((f(x+dx) - f(x)) / eps)
     return np.array(ret)
 
-# this is numerically unstable for smaller epsilon
-def num_hess(f,x,eps=1e-4):
+# this is numerically unstable
+def num_hess(f,x,eps=EPS):
     def g(y):
         return num_grad(f,y,eps)
-    return num_grad(g,x,eps*eps)
+    return num_grad(g,x,math.sqrt(eps))
+    #return num_grad(g,x,eps)
+
+def log_within(x, y, eps=1e-2, trunc=1e-12):
+    approx_zero = np.logical_or(np.abs(x) < trunc, np.abs(y) < trunc)
+    not_zero = np.logical_not(approx_zero)
+    if np.any(not_zero):
+        assert max(np.abs(np.log(x[not_zero] / y[not_zero]))) < eps
+    if np.any(approx_zero):
+        assert np.max(np.abs(x[approx_zero])) < trunc and np.max(np.abs(y[approx_zero])) < trunc
 
 def check_gradient(f, x):
     xd = np.asarray(adnumber(x))
     fxd = f(xd)
     grad1 = np.asarray(fxd.gradient(xd))
 
-    grad2 = num_grad(f, x)
+    #grad2 = num_grad(f, x)
+    grad2 = Gradient(f)(x)
 
     print x, "\n", f(x)
 
-    print grad1, "\n", grad2
-    approx_zero = np.logical_or(np.abs(grad1) < 1e-15, np.abs(grad2) < 1e-15)
-    not_zero = np.logical_not(approx_zero)
-    if np.any(not_zero):
-        assert max(np.abs(np.log(grad1[not_zero] / grad2[not_zero]))) < 1e-2 
-    if np.any(approx_zero):
-        assert np.all(grad1[approx_zero] < 1e-15) and np.all(grad2[approx_zero] < 1e-15)
+    print "gradient1\n", grad1, "\ngradient2\n", grad2
+    log_within(grad1,grad2)
 
-    # avoid doing numerical hessian for now as it is unstable
+    # note: numerical hessian can be numerically unstable!
 #     hess1 = np.asarray(map(np.asarray, fxd.hessian(xd)))
-#     hess2 = num_hess(f,x)
-#     print hess1,"\n",hess2
-#     assert np.max(np.log(hess1 / hess2)) < 1e-2
+# #     hess2 = num_hess(f,x)
+#     hess2 = Hessian(f)(x)
+
+#     print "hessian1\n", hess1,"\nhessian2\n",hess2,"\nhessian_diff\n", hess1-hess2
+#     log_within(hess1,hess2,trunc=1e-7)
 
 def test_simple():
     def f(x):
@@ -110,7 +121,7 @@ def sfs_func(demo_func, n_lins, normalized=True):
     return f
 
 @pytest.mark.parametrize("n,epochs,normalized", 
-                         ((2,1,norm) for norm in (False,True)))
+                         ((5,5,norm) for norm in (False,True)))
 def test_piecewise_constant_p(n, epochs, normalized):
     n_lins = {'a' : n}
 