b7821d0c6c3d7883e8f8523a083dc4a9d2db5183
Jack Kamm <jackkamm@gmail.com>
2015-04-10 15:45:55 -0700

    added pytest fixture for setting adarray_order to 2

--- test/test_moran_model.py
+++ test/test_moran_model.py
@@ -2,7 +2,7 @@ from __future__ import division
 import numpy as np
 import moran_model
 import pytest
-from adarray import adnumber, get_order, set_order
+from adarray import adnumber
 
 def test_rate_matrix():
     n = 4
@@ -17,30 +17,21 @@ def test_rate_matrix():
 @pytest.mark.parametrize("n,t", 
         ((n, t) for n in (5, 10, 50, 100, 250) 
             for t in (0.01, 0.1, 1.0, 10.0, 100.0) if n * t < 100))
-def test_eig_vs_expm(n, t):
+def test_eig_vs_expm(n, t, ad_order2):
     print(n, t)
     
-    prev_order = get_order()
-    set_order(2)
+    v = np.random.random(n + 1)
 
-    try:
-        v = np.random.random(n + 1)
+    x,y,z = adnumber([1,1,1])
 
-        x,y,z = adnumber([1,1,1])
+    t = x**2 * y**3 * z**4 * t
 
-        t = x**2 * y**3 * z**4 * t
+    v = x**3 * y**2 * z**3 * v
 
-        v = x**3 * y**2 * z**3 * v
+    w1 = moran_model.moran_action(t, v)
+    w2 = moran_model._old_moran_action(t, v)
+    assert np.allclose(w1.x, w2.x)
 
-        w1 = moran_model.moran_action(t, v)
-        w2 = moran_model._old_moran_action(t, v)
-        assert np.allclose(w1.x, w2.x)
+    assert np.max(np.log(np.array(w1.gradient([x,y,z])) / np.array(w2.gradient([x,y,z])))) < 1e-8
 
-        assert np.max(np.log(np.array(w1.gradient([x,y,z])) / np.array(w2.gradient([x,y,z])))) < 1e-8
-
-        assert np.max(np.log(np.array(w1.hessian([x,y,z])) / np.array(w2.hessian([x,y,z])))) < 1e-6
-
-        set_order(prev_order)
-    except Exception as e:
-        set_order(prev_order)
-        assert False, e
+    assert np.max(np.log(np.array(w1.hessian([x,y,z])) / np.array(w2.hessian([x,y,z])))) < 1e-6
