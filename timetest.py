804b252325e9c94f8596fb6364d82394c7416715
jackkamm <jackkamm@Amalthea.local>
2015-04-19 07:57:46 -0700

    autodiff time tests

--- timetest.py
+++ timetest.py
@@ -3,14 +3,16 @@ from demography import Demography
 from sum_product import SumProduct
 import time
 import numpy
+from adarray.ad import adnumber
 
 import sys
 sys.stdout = open('timetest.txt','w')
 
-def check_time(cmd, data):
+def check_time(demofunc, data, *x):
     # precompute
     start = time.time()
-    demo = Demography.from_ms(cmd)
+    #demo = Demography.from_ms(cmd)
+    demo = demofunc(*x)
     demo.totalSfsSum
     end = time.time()
     print "Precomputation: %f seconds" % (end - start)
@@ -24,8 +26,8 @@ def check_time(cmd, data):
     end = time.time()
     print "%f seconds per site (%d sites in %f seconds)" % ( (end-start) / len(data), len(data), end-start)
 
-def check_demo_time(cmd):
-    demo = Demography.from_ms(cmd)
+def check_demo_time(demofunc, *x):
+    demo = demofunc(*x)
     sumFreqs, sumSqFreqs, nonzeroFreqs = demo.simulate_sfs(num_sims=100, theta=1.0)
     
     data = []
@@ -35,15 +37,28 @@ def check_demo_time(cmd):
             state[pop] = {'derived' : key[i], 'ancestral' : demo.n_lineages_subtended_by[pop] - key[i]}
         data.append(state)
 
-    print "Timing demography %s" % cmd
-    check_time(cmd, data)
+    print "\n\nTiming demography %s\n" % demo.graph['cmd']
 
-check_demo_time("-I 2 10 10 -ej 1.0 2 1")
-check_demo_time("-I 4 10 10 10 10 -ej .4 4 3 -ej 1.0 3 2 -ej 2.0 2 1")
+    print "No autodiff\n"
+    check_time(demofunc, data, *x)
 
-cmd10 = "-I 10 %s" % (" ".join(["10"]*10))
-t = .1
-for i in reversed(range(9)):
-    cmd10 += " -ej %f %d %d" % (t, i+2, i+1)
-    t += .1
-check_demo_time(cmd10)
+    print "\nAutodiff (%d variables)\n" % len(x)
+    x = map(adnumber,x)
+    check_time(demofunc, data, *x)
+
+
+def two_pop_demo(x):
+    return Demography.from_ms("-I 2 10 10 -ej $0 2 1", x)
+check_demo_time(two_pop_demo, 1.0)
+
+def four_pop_demo(t0,t1,t2):
+    return Demography.from_ms("-I 4 10 10 10 10 -ej $0 4 3 -ej $1 3 2 -ej $2 2 1",
+                              t0,t1,t2)
+check_demo_time(four_pop_demo, 0.4, 1.0, 2.0)
+
+def ten_pop_demo(*t):
+    cmd10 = "-I 10 %s" % (" ".join(["10"]*10))
+    for i in range(9):
+        cmd10 += " -ej $%d %d %d" % (i, i+1, i+2)
+    return Demography.from_ms(cmd10, *t)
+check_demo_time(ten_pop_demo, *[.1 * i for i in range(1,10)])
