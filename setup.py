29c877ca926b33ab7d46da490fd5f2f60ac64344
Jack Kamm <jackkamm@gmail.com>
2015-05-04 15:02:36 -0700

    some cleanup

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
-
 from distutils.core import setup
-
 setup(name='momi',
-        version='0.01',
-        description='Moran model for inference',
-        author='Jack Kamm, Jonathan Terhorst, Yun S. Song',
-        author_email='terhorst@stat.berkeley.edu',
-        url='https://github.com/terhorst/momi'
-        )
+      version='0.1',
+      description='MOran Model for Inference',
+      author='Jack Kamm, Jonathan Terhorst, Yun S. Song',
+      author_email='jkamm@stat.berkeley.edu, terhorst@stat.berkeley.edu',
+      packages=['momi'],
+      install_requires=['numpy>=1.8','networkx','autograd'],
+      url='https://github.com/jackkamm/momi',
+      )
