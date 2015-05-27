a31bd5e086d0d8873926cce743c3e80402d1389b
Jack Kamm <jackkamm@gmail.com>
2015-05-27 14:39:05 -0700

    switched hua chen equations to floating point. rearranged hua chen equations to avoid unnecessary multiplication, underflow issues.

--- huachen_eqs.py
+++ huachen_eqs.py
@@ -3,14 +3,18 @@ import numpy
 import scipy.misc
 import operator
 import math
-import gmpy2
-from gmpy2 import mpz, mpfr
 from util import memoize_instance
 import warnings
 
-math_mod = gmpy2
+math_mod = math
+myint,myfloat = int,float
 
-gmpy2.get_context().precision=100
+## UNCOMMENT FOR HIGHER PRECISION
+# import gmpy2
+# math_mod = gmpy2
+# gmpy2.get_context().precision=100
+# myint,myfloat = gmpy2.mpz, gmpy2.mpfr
+
 
 '''
 Formulas from Hua Chen 2012, Theoretical Population Biology
@@ -82,9 +86,9 @@ class SumProduct_Chen(object):
                 n_right = n_ancestral_r + n_derived_r
                 if any([n_right == 0, n_right > n_leaves_r, n_left==0, n_left > n_leaves_l]):
                     continue
-                p = math.exp(logbinom(n_ancestral, n_ancestral_l) + 
-                         logbinom(n_derived, n_derived_l) - 
-                         logbinom(total_lineages, n_ancestral_l + n_derived_l))
+                p = math.exp(log_binom(n_ancestral, n_ancestral_l) + 
+                         log_binom(n_derived, n_derived_l) - 
+                         log_binom(total_lineages, n_ancestral_l + n_derived_l))
                 assert p != 0.0
                 for args in ((right_node, n_ancestral_r, n_derived_r), (left_node, n_ancestral_l, n_derived_l)):
                     p *= self.partial_likelihood_top(*args)
@@ -99,13 +103,7 @@ class SumProduct_Chen(object):
             for n_bottom in range(n_derived, n_leaves+1):
                 n_ancestral = n_bottom - n_derived
                 p_bottom = self.partial_likelihood_bottom(node, n_ancestral, n_derived)
-                if self.G.chen[node].timeLen == float('inf'):
-                    max_top = 1
-                else:
-                    max_top = n_bottom-n_derived+1
-                for n_top in range(1,max_top+1):
-                    ret += p_bottom * self.G.chen[node].ES_i(n_derived, n_bottom, n_top) * self.G.chen[node].g(n_bottom,n_top)
-
+                ret += p_bottom * self.G.chen[node].truncated_sfs(n_derived, n_bottom)
 
         if self.G.is_leaf(node):
             return ret
@@ -134,14 +132,14 @@ class SFS_Chen(object):
         self.N_diploid = N_diploid
         # precompute
         for n in range(1,max_n+1):
+            for i in range(1,n+1):
+                self.truncated_sfs(i,n)
+                
+            max_m = n
             if timeLen == float('inf'):
-                max_top = 1
-            else:
-                max_top = n
-            for m in range(1,max_top+1):
+                max_m = 1
+            for m in range(1,max_m+1):
                 self.g(n,m)
-                for i in range(1,(n-m+1)+1):
-                    self.ES_i(i,n,m)
             
     @memoize_instance    
     def g(self, n, m):
@@ -162,22 +160,33 @@ class SFS_Chen(object):
         assert n >= m
         return math.fsum([p_n_k(i, n, k) * k * self.ET(k, n, m) for k in range(m, n + 1)])
 
-def prod(l):
-    return reduce(operator.mul, l + [1])
+    @memoize_instance
+    def truncated_sfs(self, i, n):
+        max_m = n-i+1
+        if self.timeLen == float('inf'):
+            max_m = 1
+            
+        ret = 0.0
+        for m in range(1,max_m+1):
+            ret += self.ES_i(i, n, m)
+        return ret
+        
 
-def rising(n, k):
-    return prod(range(n, n + k))
+def log_factorial(n):
+    return math_mod.lgamma(n+1)
 
+def log_rising(n,k):
+    return log_factorial(n+k-1) - log_factorial(n-1)
 
-def falling(n, k):
-    return prod(range(n - k + 1, n + 1))
-
+def log_falling(n,k):
+    return log_factorial(n) - log_factorial(n-k)
 
 def gcoef(k, n, m, N_diploid, tau):
-    k, n, m = map(mpz, [k, n, m])
-    N_diploid = mpfr(N_diploid)
-    tau = mpfr(tau)
-    return (2*k - 1) * (-1)**(k - m) * rising(m, k-1) * falling(n, k) / math_mod.factorial(m) / math_mod.factorial(k - m) / rising(n, k) 
+    k, n, m = map(myint, [k, n, m])
+    N_diploid = myfloat(N_diploid)
+    tau = myfloat(tau)
+    return (2*k - 1) * (-1)**(k - m) * math_mod.exp(log_rising(m, k-1) + log_falling(n, k) - log_factorial(m) - log_factorial(k - m) - log_rising(n, k))
+    #return (2*k - 1) * (-1)**(k - m) * rising(m, k-1) * falling(n, k) / math_mod.factorial(m) / math_mod.factorial(k - m) / rising(n, k) 
 
 
 def g_sum(n, m, N_diploid, tau):
@@ -185,15 +194,11 @@ def g_sum(n, m, N_diploid, tau):
         if m == 1:
             return 1.0
         return 0.0
-    tau = mpfr(tau)
+    tau = myfloat(tau)
     return float(sum([gcoef(k, n, m, N_diploid, tau) * math_mod.exp(-k * (k - 1) * tau / 4 / N_diploid) for k in range(m, n + 1)]))
 
 
 g = g_sum
-
-def log_g(n, m, N_diploid, tau):
-    assert n >= m
-    return float(math_mod.log(g(n, m, N_diploid, tau)))
 
 def formula1(n, m, N_diploid, tau):
     def expC2(k):
@@ -201,14 +206,15 @@ def formula1(n, m, N_diploid, tau):
     r = sum(gcoef(k, n, m, N_diploid, tau) * 
             ((expC2(m) - expC2(k)) / (k - m) / (k + m - 1) - (tau / 4 / N_diploid * expC2(m)))
             for k in range(m + 1, n + 1))
-    q = 4 * N_diploid / g(n, m, N_diploid, tau)
+    #q = 4 * N_diploid / g(n, m, N_diploid, tau)
+    q = 4 * N_diploid
     return float(r * q)
 
 
 def formula3(j, n, m, N_diploid, tau):
     # Switch argument to j here to stay consistent with the paper.
-    j, n, m = map(mpz, [j, n, m])
-    tau, N_diploid = map(mpfr, [tau, N_diploid])
+    j, n, m = map(myint, [j, n, m])
+    tau, N_diploid = map(myfloat, [tau, N_diploid])
     def expC2(kk):
         return math_mod.exp(-kk * (kk - 1) / 4 / N_diploid * tau)
     r = sum(gcoef(k, n, j, N_diploid, tau) * # was gcoef(k, n, j + 1, N_diploid, tau) * 
@@ -230,7 +236,8 @@ def formula3(j, n, m, N_diploid, tau):
                 )
             for k in range(j + 1, n + 1)
             )
-    q = 4 * N_diploid / mpfr(g(n, m, N_diploid, tau))
+    #q = 4 * N_diploid / myfloat(g(n, m, N_diploid, tau))
+    q = 4 * N_diploid
     return float(q * r)
 
 def formula2(n, m, N_diploid, tau):
@@ -239,7 +246,8 @@ def formula2(n, m, N_diploid, tau):
     r = sum(gcoef(k, n, m, N_diploid, tau) * 
             ((expC2(k) - expC2(n)) / (n - k) / (n + k - 1) - (tau / 4 / N_diploid * expC2(n)))
             for k in range(m, n))
-    q = 4 * N_diploid / g(n, m, N_diploid, tau)
+    #q = 4 * N_diploid / g(n, m, N_diploid, tau)
+    q = 4 * N_diploid
     return float(r * q)
 
 def ET(i, n, m, N_diploid, tau):
@@ -249,9 +257,9 @@ def ET(i, n, m, N_diploid, tau):
     if tau == float("inf"):
         if m != 1 or i == 1:
             return 0.0
-        return 2 * N_diploid / float(nChoose2(i))
+        return 2 * N_diploid / float(nChoose2(i)) * g(n, m, N_diploid, tau)
     if n == m:
-        return tau * (i == n)
+        return tau * (i == n) * g(n, m, N_diploid, tau)
     if m == i:
         return formula1(n, m, N_diploid, tau)
     elif n == i:
@@ -264,24 +272,21 @@ def p_n_k(i, n, k):
         return int(i == n)
     else:
         #return scipy.misc.comb(n-i-1,k-2) / scipy.misc.comb(n-1,k-1)
-        return math.exp(logbinom(n - i - 1, k - 2) - logbinom(n - 1, k - 1))
+        return math.exp(log_binom(n - i - 1, k - 2) - log_binom(n - 1, k - 1))
 
 def nChoose2(n):
     return (n * (n-1)) / 2
 
-def logfact(n):
-    return math.lgamma(n + 1)
-
-def logbinom(n, k):
+def log_binom(n, k):
     if k < 0 or k > n:
         return -float('inf')
-    return logfact(n) - logfact(n - k) - logfact(k)
+    return log_factorial(n) - log_factorial(n - k) - log_factorial(k)
 
 def log_urn_prob(n_parent_derived, n_parent_ancestral, n_child_derived, n_child_ancestral):
     n_parent = n_parent_derived + n_parent_ancestral
     n_child = n_child_derived + n_child_ancestral
     if n_child_derived >= n_parent_derived and n_parent_derived > 0 and n_child_ancestral >= n_parent_ancestral and n_parent_ancestral > 0:
-        return logbinom(n_child_derived - 1, n_parent_derived - 1) + logbinom(n_child_ancestral - 1, n_parent_ancestral - 1) - logbinom(n_child-1, n_parent-1)
+        return log_binom(n_child_derived - 1, n_parent_derived - 1) + log_binom(n_child_ancestral - 1, n_parent_ancestral - 1) - log_binom(n_child-1, n_parent-1)
     elif n_child_derived == n_parent_derived == 0 or n_child_ancestral == n_parent_ancestral == 0:
         return 0.0
     else:
