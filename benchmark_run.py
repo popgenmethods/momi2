77051a243f47f369552007aec0633f07ffdd1551
Jack Kamm <jackkamm@gmail.com>
2015-07-06 10:16:51 -0700

    more benchmarks

--- benchmark_run.py
+++ benchmark_run.py
@@ -1,8 +1,5 @@
 from subprocess import call
 
-cores = 20
-reps = 20
-
 out_file = open("benchmark.out",'a',0)
 
 def do_runs(n_total_log2):
@@ -14,5 +11,15 @@ def do_runs(n_total_log2):
 
         call(cmd, shell=True, stdout=out_file, stderr=out_file)
 
-for i in range(1, 9):
-    do_runs(i)
+cores = 20
+reps = 20
+#for i in range(1, 9):
+#    do_runs(i)
+
+cores=5
+for n_demes in [32,64,128,256]:
+    n_per_deme = int(256/n_demes)
+    cmd = "./benchmark.py %d %d %d %d --moranOnly" % (n_demes, n_per_deme, reps, cores) 
+    out_file.write(cmd + "\n")
+
+    call(cmd, shell=True, stdout=out_file, stderr=out_file)
--- plot.R
+++ plot.R
@@ -2,6 +2,7 @@ library(RSQLite)
 library(ggplot2)
 library(data.table)
 library(reshape2)
+library(plyr)
 
 db <- dbConnect(SQLite(), ".bench.db")
 dbq <- function(...) { dbGetQuery(db, ...) }
@@ -18,19 +19,27 @@ dt$model[dt$model == 'moran'] <- 'momi'
 dt$initial <- factor(dt$site == 0)
 levels(dt$initial) <- c("Per SNP", "Precomputation")
 
-dt.summary <- dt[, list(avg_t=mean(time),runs=length(time),negs=sum(result < 0,na.rm=TRUE),nans=sum(is.na(result))), by=list(model, n, D, initial)]
+rename(dt, c('model'='method'))
 
-p <- ggplot(dt.summary, aes(x=n, y=avg_t, color=D)) + geom_line() + geom_point() + ylab("Seconds") + theme(legend.position="top") + facet_grid(initial ~ model, scales='free_y')
+dt.summary <- dt[, list(avg_t=mean(time),runs=length(time),negs=sum(result < 0,na.rm=TRUE),nans=sum(is.na(result))), by=list(method, n, D, initial)]
+
+p <- ggplot(dt.summary, aes(x=n, y=avg_t, color=D)) + geom_line() + geom_point() + ylab("Seconds") + theme(legend.position="top") + facet_grid(initial ~ method, scales='free_y')
 plot(p)
 ggsave('figures/timing.pdf')
 
-p <- ggplot(dt.summary, aes(x=n, y=avg_t, color=D, linetype=model)) + geom_line() + geom_point(aes(shape=model)) + ylab("Seconds") + theme(legend.position="top") + facet_grid(initial ~ .) + scale_y_log10(breaks=c(1e-4,1e-2,1,1e2,1e4)) + scale_x_log10(breaks=c(2,4,8,16,32,64,128,256))
+p <- ggplot(dt.summary, aes(x=n, y=avg_t, color=D, linetype=method)) + geom_line() + geom_point(aes(shape=method)) + ylab("Seconds") + theme(legend.position="top") + facet_grid(initial ~ .) + scale_y_log10(breaks=c(1e-4,1e-2,1,1e2,1e4)) + scale_x_log10(breaks=c(2,4,8,16,32,64,128,256))
 plot(p)
 ggsave('figures/timing_log.pdf')
 
+write.table(format(dcast(subset(dt.summary, initial=='Per SNP'), formula=n+D~method, value.var='avg_t'),digits=4,scientific=T),
+            file='per_snp.txt', quote=F, row.names=F, sep=' & ', eol=' \\\\ \\hline \n')
+
+write.table(format(dcast(subset(dt.summary, initial=='Precomputation'), formula=n+D~method, value.var='avg_t'),digits=4,scientific=T),
+            file='precomputation.txt', quote=F, row.names=F, sep=' & ', eol=' \\\\ \\hline \n')
+
 #dt$transformed.result <- sign(dt$result) * log10(1 + abs(dt$result))
 
-dt2 <- dcast(dt, n + D + site + run_id + state + tree + initial ~ model, value.var = "result")
+dt2 <- dcast(dt, n + D + site + run_id + state + tree + initial ~ method, value.var = "result")
 dt2 <- dt2[sample(nrow(dt2)),]
 
 library(scales)
@@ -42,4 +51,7 @@ plot(p)
 ggsave('figures/accuracy.pdf')
 
 p <- ggplot(dt2, aes(x=momi,y=Chen,color=n)) + geom_abline(color='red', linetype='dashed') + geom_point() + scale_y_continuous(trans = sgn_log_trans()) + scale_x_continuous(trans = sgn_log_trans()) + facet_grid(D ~ n)
+plot(p)
+
+p <- ggplot(subset(dt2, n <= 64), aes(x=momi,y=Chen,color=n)) + geom_abline(color='red', linetype='dashed') + geom_point() + scale_y_continuous(trans = sgn_log_trans()) + scale_x_continuous(trans = sgn_log_trans()) + facet_grid(D ~ n)
 plot(p)
