KL divergence. Renamed to SfsLikelihoodSurface. fixed tutorial.py.

momi/data_structure.py
@@ -172,8 +172,30 @@ class Configs(tuple):
         except UnboundLocalError:
             pass
         return vecs, idxs
+
+class _AbstractSfs(object):
+    @property
+    @memoize_instance
+    def _counts_i(self):
+        return np.einsum("ij->i", self._counts_ij)
+
+    @property
+    @memoize_instance
+    def _counts_j(self):
+        return np.einsum("ij->j", self._counts_ij)
+
+    @property
+    @memoize_instance
+    def _total_count(self):
+        return np.sum(self._counts_j)
     
-class Sfs(object):
+    @property
+    @memoize_instance
+    def _entropy(self):
+        p = self._counts_j / float(self._total_count)
+        return np.sum(p * np.log(p))
+    
+class Sfs(_AbstractSfs):
     """
     Represents an observed SFS across several loci.
 
@@ -295,12 +317,7 @@ class Sfs(object):
                 except KeyError:
                     pass
         return counts_ij
-
-    @property
-    @memoize_instance
-    def _counts_i(self):
-        return np.einsum("ij->i", self._counts_ij)
-
+    
 class _ConfigDict(Counter):
     def __init__(self, *args, **kwargs):
         self.immutable = False

momi/likelihood.py
@@ -1,10 +1,10 @@
 
-from .util import make_constant, _maximize, _npstr, truncate0, check_psd, memoize_instance, mypartial, _get_stochastic_optimizer, count_calls
+from .util import make_constant, _minimize, _npstr, truncate0, check_psd, memoize_instance, mypartial, _get_stochastic_optimizer, count_calls
 import autograd.numpy as np
 from .compute_sfs import expected_sfs, expected_total_branch_len
 from .math_functions import einsum2, inv_psd
 from .demography import DemographyError, Demography
-from .data_structure import _hashable_config, Sfs
+from .data_structure import _hashable_config, Sfs, _AbstractSfs
 import scipy, scipy.stats
 import autograd
 from autograd import grad, hessian_vector_product, hessian
@@ -12,7 +12,7 @@ from collections import Counter
 #from functools import partial
 import random
 
-class CompositeLogLikelihood(object):
+class SfsLikelihoodSurface(object):
     def __init__(self, data, demo_func=None, mut_rate=None, folded=False, error_matrices=None, truncate_probs=1e-100, batch_size=200):
         self.data = data
         
@@ -30,32 +30,26 @@ class CompositeLogLikelihood(object):
         self.sfs_batches = _build_sfs_batches(self.sfs, batch_size)
         #self.sfs_batches = [self.sfs]
 
-    def evaluate(self, x):
-        if self.demo_func:
-            demo = self.demo_func(*x)
-        else:
-            demo = x
-        #return _composite_log_likelihood(data=self.sfs, demo=demo, mut_rate=self.mut_rate, truncate_probs=self.truncate_probs,
-        #                                 vector=False, folded=self.folded, error_matrices=self.error_matrices)
+    def log_likelihood(self, x):
+        ret = _batched_log_likelihood(x, self.sfs_batches, self.demo_func, self.sfs, self.mut_rate,
+                                      truncate_probs=self.truncate_probs, folded=self.folded, error_matrices=self.error_matrices)
+        
+        return ret
 
-        G,(diff_keys,diff_vals) = demo._get_graph_structure(), demo._get_differentiable_part()
-        ret = 0.0
-        for batch in self.sfs_batches:
-            ret = ret + _prim_log_lik(diff_vals, diff_keys, G, batch, self.truncate_probs, self.folded, self.error_matrices)
-
+    def kl_divergence(self, x):
+        ret = -self.log_likelihood(x) + self.sfs._total_count * self.sfs._entropy
         if self.mut_rate:
-            ret = ret + _mut_factor(self.sfs, demo, self.mut_rate, False)
+            ## KL divergence is for empirical distribution over all sites (monomorphic and polymorphic)
+            ## taking the limit so that each site has infinitessimal width
+            counts_i = self.sfs._counts_i
+            ret = ret + np.sum(-counts_i + counts_i * np.log(counts_i))
+
+        ret = ret / float(self.sfs._total_count)
+        assert ret >= 0
 
         return ret
-
-    def gradient(self, x):
-        _prim_log_lik_grad.reset_count()
-        ret = grad(self.evaluate)(x)
-        ## make sure that autograd is making use of _prim_log_lik_grad
-        assert _prim_log_lik_grad.num_calls() == len(self.sfs_batches)
-        return ret
-            
-    def find_maximum(self, x0, maxiter=100, bounds=None, method="newton", output_progress = False, **kwargs):
+    
+    def find_optimum(self, x0, maxiter=100, bounds=None, method="newton", output_progress = False, **kwargs):
         if method=="newton":
             return self.newton(x0, maxiter, bounds, output_progress, **kwargs)
         elif method=="adam":
@@ -68,16 +62,16 @@ class CompositeLogLikelihood(object):
     
     def newton(self, x0, maxiter, bounds, output_progress,
                xtol=-1, ftol=-1, gtol=-1, finite_diff_eps=None):
-        f = self.evaluate
+        f = self.kl_divergence
 
         options = {'ftol':ftol,'xtol':xtol,'gtol':gtol}
 
-        if not finite_diff_eps: jac = self.gradient
+        if not finite_diff_eps: jac = True
         else:
-            jac = None
+            jac = False
             options['eps'] = finite_diff_eps
 
-        return _maximize(f=f, start_params=x0, jac=jac, method="tnc", maxiter=maxiter, bounds=bounds, options=options, output_progress=output_progress)
+        return _minimize(f=f, start_params=x0, jac=jac, hess=False, hessp=False, method="tnc", maxiter=maxiter, bounds=bounds, options=options, output_progress=output_progress, f_name="KL-Divergence")
         
     def adam(self, x0, maxiter, bounds, output_progress,
              n_chunks, **kwargs):
@@ -86,7 +80,7 @@ class CompositeLogLikelihood(object):
             raise NotImplementedError("adam not yet implemented for finite SFS batch_size -- set batch_size=float('inf') in constructor")
         start_params = np.array(x0)
         
-        f = lambda minibatch, params, minibatch_mut_rate: _composite_log_likelihood(minibatch, self.demo_func(*params), minibatch_mut_rate, truncate_probs = self.truncate_probs, folded=self.folded, error_matrices=self.error_matrices)
+        f = lambda minibatch, params, minibatch_mut_rate: -_composite_log_likelihood(minibatch, self.demo_func(*params), minibatch_mut_rate, truncate_probs = self.truncate_probs, folded=self.folded, error_matrices=self.error_matrices)
         
         sgd_fun = _get_stochastic_optimizer("adam")
         
@@ -132,15 +126,41 @@ def _build_sfs_batches(sfs, batch_size):
 
     idx_list = [sorted_idxs[s] for s in slices]
     
-    counts_j = np.einsum("ij->j", sfs._counts_ij)
     counts_j_list = []
     for idx in idx_list:
-        curr = np.zeros(len(counts_j))
-        curr[idx] = counts_j[idx]
+        curr = np.zeros(len(sfs._counts_j))
+        curr[idx] = sfs._counts_j[idx]
         counts_j_list.append(curr)
 
     return [_SubSfs(sfs.configs, cj) for cj in counts_j_list]
+
+
+def _batched_log_likelihood(x, sfs_batches, demo_func, sfs, mut_rate, **kwargs):
+    if demo_func:
+        demo = demo_func(*x)
+    else:
+        demo = x
+
+    G,(diff_keys,diff_vals) = demo._get_graph_structure(), demo._get_differentiable_part()
+    ret = 0.0
+    for batch in sfs_batches:
+        ret = ret + _prim_log_lik(diff_vals, diff_keys, G, batch, **kwargs)
+
+    if mut_rate:
+        ret = ret + _mut_factor(sfs, demo, mut_rate, False)
+
+    return ret
+_batched_grad_helper = grad(_batched_log_likelihood)
+def _batched_log_lik_grad(x, sfs_batches, *args,**kwargs):
+    ## make sure that autograd is making use of _prim_log_lik_grad    
+    _prim_log_lik_grad.reset_count()
+    ret = _batched_grad_helper(x, sfs_batches, *args, **kwargs)
+    assert _prim_log_lik_grad.num_calls() == len(sfs_batches)
+    return ret
     
+_batched_log_likelihood = autograd.primitive(_batched_log_likelihood)
+_batched_log_likelihood.defgrad(lambda ans, *args, **kwargs: lambda g: g * _batched_log_lik_grad(*args, **kwargs))
+
     
 def _prim_log_lik(diff_vals, diff_keys, G, data, truncate_probs, folded, error_matrices):
     demo = Demography(G, diff_keys, diff_vals)
@@ -148,7 +168,7 @@ def _prim_log_lik(diff_vals, diff_keys, G, data, truncate_probs, folded, error_m
 _prim_log_lik_grad = count_calls(grad(_prim_log_lik))
 
 _prim_log_lik = autograd.primitive(_prim_log_lik)
-_prim_log_lik.defgrad(lambda ans, diff_vals, *args: lambda g: tuple(g*y for y in _prim_log_lik_grad(diff_vals, *args)))
+_prim_log_lik.defgrad(lambda ans, diff_vals, *args, **kwargs: lambda g: tuple(g*y for y in _prim_log_lik_grad(diff_vals, *args, **kwargs)))
     
 def _composite_log_likelihood(data, demo, mut_rate=None, truncate_probs = 0.0, vector=False, **kwargs):
     """
@@ -422,9 +442,8 @@ class _SubConfigs(object):
         idxs[denom_idx_key] = old_2_new_idxs[denom_idx]
         return old_idxs, idxs
 
-class _SubSfs(object):
+class _SubSfs(_AbstractSfs):
     ## represents a subsample of SFS
-    ## Just used by stochastic gradient descent for now
     def __init__(self, configs, counts):
         assert len(counts.shape) == 1 and len(counts) == len(configs)
         
@@ -433,7 +452,6 @@ class _SubSfs(object):
         
         counts = counts[counts != 0]
         self._counts_ij = np.array(counts, ndmin=2)
-        self._counts_i = np.einsum("ij->i",self._counts_ij)
 
 
 ### stuff for confidence intervals

momi/util.py
@@ -148,7 +148,7 @@ def smooth_pos_map(x):
 #             return f
 #     return func
 
-def wrap_generic_maximizer(generic_minimizer):
+def wrap_minimizer(minimizer):
     def wrapped(f, start_params, maxiter, bounds = None, **kwargs):
         fixed_params = []    
         if bounds is not None:
@@ -171,27 +171,30 @@ def wrap_generic_maximizer(generic_minimizer):
 
             fixed_offset = np.dot(proj0, fixed_offset)
 
-            f0 = lambda x,*fargs,**fkwargs: f(np.dot(proj1, x) + fixed_offset, *fargs, **fkwargs)
+            get_x = lambda x0: np.dot(proj1, x0) + fixed_offset
+
+            def subfun(fun):
+                if fun is None: return None
+                new_fun = lambda x0, *fargs, **fkwargs: fun(get_x(x0), *fargs, **fkwargs)
+                functools.update_wrapper(new_fun, fun)
+                return new_fun
+            f0 = subfun(f)
+            
             start0 = np.array([s for (fxd,s) in zip(fixed_idxs,start_params) if not fxd])
             bounds0 = [b for (fxd,b) in zip(fixed_idxs, bounds) if not fxd]
-            ret = generic_minimizer(f0, start0, maxiter, bounds0, **kwargs)
+            ret = minimizer(f0, start0, maxiter, bounds0, **kwargs)
             ret.x = np.dot(proj1,ret.x) + fixed_offset
         else:
-            ret = generic_minimizer(f, start_params, maxiter, bounds, **kwargs)
-        for k,v in list(ret.items()):
-            if k in ("fun","jac") or k.startswith("hess"):
-                ret[k] = -v
-            else:
-                ret[k] = v
+            ret = minimizer(f, start_params, maxiter, bounds, **kwargs)
         return ret
-    functools.update_wrapper(wrapped, generic_minimizer)
+    functools.update_wrapper(wrapped, minimizer)
     return wrapped
 
-@wrap_generic_maximizer
-def _maximize(f, start_params, maxiter, bounds,
-             jac = None, hess = None, hessp = None,
-             method = 'tnc', tol = None, options = {},
-             output_progress = False, **kwargs):        
+@wrap_minimizer
+def _minimize(f, start_params, maxiter, bounds,
+              jac = True, hess = False, hessp = False,
+              method = 'tnc', tol = None, options = {},
+              output_progress = False, f_name="objective", **kwargs):        
     if 'maxiter' in options:
         raise ValueError("Please specify maxiter thru function argument 'maxiter', rather than 'options'")
     
@@ -199,18 +202,18 @@ def _maximize(f, start_params, maxiter, bounds,
     options['maxiter'] = maxiter
    
     kwargs = dict(kwargs)
-    kwargs.update({kw : wrap_objective(df,kw)
-                   for kw, df in [('jac', jac), ('hessp', hessp), ('hess', hess)]
-                   if df})
+    kwargs.update({kw : wrap_objective(d(f),kw)
+                   for kw,d,b in [('jac', grad, jac), ('hessp', hessian_vector_product, hessp), ('hess', hessian, hess)]
+                   if b})
 
-    f = wrap_objective(f, "objective", check_inf=False, output_progress=output_progress)
+    f = wrap_objective(f, f_name, check_inf=False, output_progress=output_progress)
     
     return scipy.optimize.minimize(f, start_params, method=method, bounds=bounds, tol=tol, options=options, **kwargs)
 
 def wrap_objective(fun,name, check_inf=True, output_progress=False):
     def new_fun(*a, **kw):
         try:
-            ret = -fun(*a,**kw)
+            ret = fun(*a,**kw)
             if np.any(np.isnan(ret)) or (check_inf and not np.all(np.isfinite(ret))):
                 raise OptimizationError("%s ( %s ) == %s. (Consider setting stricter bounds? e.g. set a lower bound of 1e-100 instead of 0)" % (name,str(*a),str(ret)))
             return ret
@@ -220,7 +223,7 @@ def wrap_objective(fun,name, check_inf=True, output_progress=False):
     if output_progress:
         new_fun = _verbosify(new_fun,
                              before = lambda i,x,*a,**kw: "evaluation %d" % i,
-                             after = lambda i,ret,x,*a,**kw: "%s ( %s ) == %g" % (name, _npstr(x), -ret),
+                             after = lambda i,ret,x,*a,**kw: "%s ( %s ) == %g" % (name, _npstr(x), ret),
                              print_freq = output_progress)
 
     functools.update_wrapper(new_fun, fun)
@@ -278,7 +281,7 @@ def wrap_sgd(optimizer):
     return wrapped_optimizer
         
 ## based on code from autograd/examples
-@wrap_generic_maximizer
+@wrap_minimizer
 @wrap_sgd
 def adam(fun_and_jac_list, start_params, maxiter, bounds,
          tol=None,
@@ -313,7 +316,7 @@ def adam(fun_and_jac_list, start_params, maxiter, bounds,
             x = np.maximum(np.minimum(x, upper_bounds), lower_bounds)
     return scipy.optimize.OptimizeResult({'x':x, 'fun':f, 'jac':g, 'history':history})
 
-@wrap_generic_maximizer
+@wrap_minimizer
 @wrap_sgd
 def adadelta(fun_and_jac_list, start_params, maxiter, bounds,
              tol=None,

test/test_inference.py
@@ -3,7 +3,7 @@ import os, random
 import autograd.numpy as np
 from autograd import grad
 
-from momi import make_demography, simulate_ms, CompositeLogLikelihood
+from momi import make_demography, simulate_ms, SfsLikelihoodSurface
 import momi
 from test_ms import ms_path, scrm_path
 
@@ -23,7 +23,7 @@ def test_archaic_sample():
                       num_loci=num_runs, mut_rate=theta, cmd_format='scrm').sfs
     
     x0 = np.array([random.uniform(0,join_time)])
-    res = CompositeLogLikelihood(sfs, demo_func=get_demo).find_maximum(x0, bounds=[(0,join_time)])
+    res = SfsLikelihoodSurface(sfs, demo_func=get_demo).find_optimum(x0, bounds=[(0,join_time)])
     
     print res.jac
     assert abs(res.x - true_sample_t) < .1
@@ -61,7 +61,7 @@ def check_jointime_inference(folded, add_n, finite_diff_eps=0):
     
     x0 = np.array([random.uniform(0,t1)])
     bound_eps = finite_diff_eps + 1e-12
-    res = CompositeLogLikelihood(sfs, get_demo, folded=folded).find_maximum(x0, bounds=[(bound_eps,t1-bound_eps),], finite_diff_eps=finite_diff_eps)
+    res = SfsLikelihoodSurface(sfs, get_demo, folded=folded).find_optimum(x0, bounds=[(bound_eps,t1-bound_eps),], finite_diff_eps=finite_diff_eps)
     
     print res.jac
     assert abs(res.x - t0) / t0 < .05
@@ -82,7 +82,7 @@ def test_underflow_robustness(folded):
         sfs = sfs.fold()
     
     #optimize_res = composite_mle_search(sfs, get_demo, np.array([np.log(0.1),np.log(100.0)]), mu, hessp=True, method='newton-cg', sfs_kwargs={'folded':folded})
-    optimize_res = CompositeLogLikelihood(sfs, get_demo, mut_rate=mu, folded=folded).find_maximum(np.array([np.log(0.1),np.log(100.0)]))
+    optimize_res = SfsLikelihoodSurface(sfs, get_demo, mut_rate=mu, folded=folded).find_optimum(np.array([np.log(0.1),np.log(100.0)]))
     print optimize_res
     
     inferred_x = optimize_res.x

test/test_likelihood.py
@@ -2,7 +2,7 @@
 import pytest
 import momi
 import momi.likelihood
-from momi import CompositeLogLikelihood
+from momi import SfsLikelihoodSurface
 from demo_utils import simple_five_pop_demo
 
 import autograd.numpy as np
@@ -23,7 +23,7 @@ def test_batches():
 
     assert sfs_len > 30
 
-    assert np.isclose(CompositeLogLikelihood(sfs, batch_size=5).evaluate(demo),
+    assert np.isclose(SfsLikelihoodSurface(sfs, batch_size=5).log_likelihood(demo),
                       momi.likelihood._composite_log_likelihood(sfs, demo))
 
 def test_batches_grad():
@@ -41,5 +41,5 @@ def test_batches_grad():
 
     assert sfs_len > 30
 
-    assert np.allclose(CompositeLogLikelihood(sfs, batch_size=5, demo_func=demo_func).gradient(x0),
+    assert np.allclose(-sfs._total_count * grad(SfsLikelihoodSurface(sfs, batch_size=5, demo_func=demo_func).kl_divergence)(x0),
                        grad(lambda x: momi.likelihood._composite_log_likelihood(sfs, demo_func(*x)))(x0))

test/test_stochastic_gradient.py
@@ -128,7 +128,7 @@ def test_stochastic_inference(folded):
     if folded:
         sfs = sfs.fold()
     
-    optimize_res = momi.CompositeLogLikelihood(sfs, demo_func=get_demo, mut_rate=mu, folded=folded).find_maximum(np.array([.1,.9]), bounds=[(1e-100,None),(1e-100,None)], method="adam", n_chunks=10, output_progress=10, maxiter=10)
+    optimize_res = momi.SfsLikelihoodSurface(sfs, demo_func=get_demo, mut_rate=mu, folded=folded).find_optimum(np.array([.1,.9]), bounds=[(1e-100,None),(1e-100,None)], method="adam", n_chunks=10, output_progress=10, maxiter=10)
     print optimize_res
     
     inferred_x = optimize_res.x