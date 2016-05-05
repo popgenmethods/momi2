fixed entropy/kl-divergence for missing data

momi/data_structure.py
@@ -67,7 +67,9 @@ class Configs(object):
         self.has_missing_data = np.any(config_sampled_n != self.sampled_n)
 
         self._index_dict = {c:i for i,c in enumerate(self.config_tuples)}
-       
+        self.has_monomorphic = _has_monomorphic(self.config_array)
+
+        
     def __getitem__(self, i): return self.config_tuples[i]
     def __len__(self): return len(self.config_tuples)
     def get_index(self, config): return self._index_dict[_config_tuple(config)]
@@ -274,9 +276,23 @@ class Sfs(object):
     @property
     @memoize_instance
     def _entropy(self):
-        _,counts = self._idxs_counts(None)
-        p = counts / float(self.n_snps())
-        return np.sum(p * np.log(p))
+        counts = self._total_freqs
+        n_snps = float(self.n_snps())
+        p = counts / n_snps
+        #return np.sum(p * np.log(p))
+        ret = np.sum(p * np.log(p))
+
+        ## correct for missing data        
+        sampled_n = np.sum(self.configs.config_array, axis=2)
+        sampled_n_counts = Counter()
+        assert len(counts) == len(sampled_n)
+        for c,n in zip(counts, sampled_n):
+            n = tuple(n)
+            sampled_n_counts[n] += c
+        sampled_n_counts = np.array(list(sampled_n_counts.values()), dtype=float)
+
+        ret = ret + np.sum(sampled_n_counts / n_snps * np.log(n_snps / sampled_n_counts))
+        return ret
 
         
     def fold(self):
@@ -445,6 +461,9 @@ def _sfs_subset(configs, counts):
 
     return Sfs([{i:c for i,c in enumerate(counts)}], sub_configs)
 
+def _has_monomorphic(config_array):
+    return np.any(np.sum(config_array,axis=1) == 0)
+    
 class _Configs_Subset(object):
     ## Efficient access to subset of configs
     def __init__(self, configs, sub_idxs):
@@ -452,10 +471,11 @@ class _Configs_Subset(object):
         self.full_configs = configs
         for a in ("sampled_n", "sampled_pops", "has_missing_data"):
             setattr(self, a, getattr(self.full_configs, a))
-
-    # @property
-    # def config_array(self):
-    #     return self.full_configs.config_array[self.sub_idxs,:,:]
+        self.has_monomorphic = _has_monomorphic(self.config_array)
+            
+    @property
+    def config_array(self):
+        return self.full_configs.config_array[self.sub_idxs,:,:]
             
     def _vecs_and_idxs(self, folded):
         vecs,_ = self.full_configs._vecs_and_idxs(folded)

momi/likelihood.py
@@ -60,7 +60,7 @@ class SfsLikelihoodSurface(object):
             ret = ret + np.sum(-counts_i + counts_i * np.log(np.sum(counts_i) * mu / float(np.sum(mu))))
 
         ret = ret / float(self.sfs.n_snps())
-        assert ret >= 0, "kl-div: %f, log_lik: %f, total_count: %d" % (ret, log_lik, int(self.sfs.n_snps()))
+        assert ret >= 0, "kl-div: %s, log_lik: %s, total_count: %s" % (str(ret), str(log_lik), str(self.sfs.n_snps()))
 
         return ret
     
@@ -269,6 +269,8 @@ def _composite_log_likelihood(data, demo, mut_rate=None, truncate_probs = 0.0, v
     except AttributeError:
         sfs = data
 
+    if sfs.configs.has_monomorphic:
+        raise ValueError("Need to remove monomorphic sites from dataset before computing likelihood")
     sfs_probs = np.maximum(expected_sfs(demo, sfs.configs, normalized=True, **kwargs),
                            truncate_probs)
     log_lik = sfs._integrate_sfs(np.log(sfs_probs), vector=vector)

test/test_inference.py
@@ -34,16 +34,18 @@ def test_archaic_sample():
 @pytest.mark.parametrize("folded,add_n",
                          ((f,n) for f in (True,False) for n in (0,3)))
 def test_jointime_inference(folded, add_n):
-    check_jointime_inference(folded, add_n)
+    check_jointime_inference(folded=folded, add_n=add_n)
 
 def test_nodiff():
-    return check_jointime_inference(False, 0, finite_diff_eps=1e-8)
-    
-def check_jointime_inference(folded, add_n, finite_diff_eps=0):
-    theta=.1
+    return check_jointime_inference(finite_diff_eps=1e-8)
+
+def test_missing_data():
+    return check_jointime_inference(missing_p=.5, folded=True, num_runs=100, theta=10.)
+
+def check_jointime_inference(folded=False, add_n=0, finite_diff_eps=0, missing_p=0,
+                             use_theta=False, theta=.1, num_runs = 10000):
     t0=random.uniform(.25,2.5)
     t1= t0 + random.uniform(.5,5.0)
-    num_runs = 10000
 
     def get_demo(join_time):
         return make_demography([('-ej', join_time, 1, 2), ('-ej', t1, 2, 3)],
@@ -54,8 +56,26 @@ def check_jointime_inference(folded, add_n, finite_diff_eps=0):
     #                       true_demo.sampled_pops,
     #                       np.array(true_demo.sampled_n) - add_n)
     true_demo = true_demo.copy(sampled_n = np.array(true_demo.sampled_n) - add_n)
-    sfs = simulate_ms(ms_path, true_demo.rescaled(),
-                      num_loci=num_runs, mut_rate=theta).sfs
+    data = simulate_ms(ms_path, true_demo.rescaled(),
+                       num_loci=num_runs, mut_rate=theta)
+    if missing_p:
+        subsampled_data = []
+        for loc in range(data.n_loci):
+            curr = np.array(list(data[loc]), dtype=int)
+            if len(curr) == 0:
+                subsampled_data.append(list(curr))
+                continue
+            curr = np.random.binomial(curr, 1.0-missing_p)
+            curr_polymorphic = np.all(np.sum(curr, axis=1) != 0, axis=1)
+            curr = curr[curr_polymorphic,:,:]
+            subsampled_data.append(curr)
+        assert any([not np.array_equal(subsampled_data[loc],
+                                       np.array(list(data[loc]), dtype=int))
+                    for loc in range(data.n_loci)])
+        data = momi.make_seg_sites_data(data.sampled_pops, subsampled_data)
+
+    sfs = data.sfs
+    assert sfs.n_snps() > 0
     sfs = sfs._copy(sampled_n=np.array(true_demo.sampled_n)+add_n)
     if folded:
         sfs = sfs.fold()
@@ -66,9 +86,12 @@ def check_jointime_inference(folded, add_n, finite_diff_eps=0):
     prim_log_lik.reset_grad_count()
     assert not prim_log_lik.num_grad_calls()
 
+    if not use_theta:
+        theta = None
+    
     x0 = np.array([random.uniform(0,t1)])
     bound_eps = finite_diff_eps + 1e-12
-    res = SfsLikelihoodSurface(sfs, get_demo, folded=folded).find_optimum(x0, bounds=[(bound_eps,t1-bound_eps),], finite_diff_eps=finite_diff_eps)
+    res = SfsLikelihoodSurface(sfs, get_demo, mut_rate=theta, folded=folded).find_optimum(x0, bounds=[(bound_eps,t1-bound_eps),], finite_diff_eps=finite_diff_eps)
     
     #res = SfsLikelihoodSurface(sfs, get_demo, folded=folded).find_optimum(x0, bounds=[(0,t1),])
 