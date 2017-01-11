743a87bc44ec74d2d679adf6dabc5a1dbdc3d677
Jack Kamm <jk21@sanger.ac.uk>
2017-01-11 17:11:53 +0000

    added read_plink function

--- momi/__init__.py
+++ momi/__init__.py
@@ -13,3 +13,4 @@ from .likelihood import SfsLikelihoodSurface
 from .confidence_region import ConfidenceRegion
 from .tensor import sfs_tensor_prod
 from .data_structure import write_seg_sites, read_seg_sites, seg_site_configs, site_freq_spectrum, config_array
+from .read_plink import read_plink_frq_strat
