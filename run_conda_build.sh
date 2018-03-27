427395c62a0fe4061c9fe304b1af054158c218a6
Jack Kamm <jackkamm@gmail.com>
2018-03-27 07:47:02 +0100

    conda

--- run_conda_build.sh
+++
@@ -1,3 +0,0 @@
-# defaults channel MUST be higher priority than conda-forge or bad things happen!
-# e.g., https://github.com/conda/conda-build/issues/2523#issuecomment-346963138
-conda build conda/ -c defaults -c conda-forge -c bioconda
