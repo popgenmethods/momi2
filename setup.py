2ac486e44fcdaeab9c124e2e379f6f8ca5be366c
Jack Kamm <jackkamm@gmail.com>
2022-11-12 20:58:44 -0800

    Bump version to 2.1.20

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ if not on_rtd:
         'pandas', 'msprime', "matplotlib>=3.3", "seaborn", "pysam"])
 
 setup(name='momi',
-      version='2.1.19',
+      version='2.1.20',
       description='MOran Model for Inference',
       author='Jack Kamm, Jonathan Terhorst, Richard Durbin, Yun S. Song',
       author_email='jkamm@stat.berkeley.edu, terhorst@stat.berkeley.edu, yss@eecs.berkeley.edu',
